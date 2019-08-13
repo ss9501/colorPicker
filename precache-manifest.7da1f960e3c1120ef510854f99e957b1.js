86eaf612b51fe6ea2b7dbf284a68bb2c9adef76e
ss9501 <1162882015@qq.com>
2019-08-13 22:00:22 +0800

    v2

---
+++ precache-manifest.7da1f960e3c1120ef510854f99e957b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275ad82c09e3632fe243b49e6f02b221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5921195339c77ff4403d",
+    "url": "/static/css/2.fb6ed098.chunk.css"
+  },
+  {
+    "revision": "c71808282eb5beabf435",
+    "url": "/static/css/main.ca44a0e7.chunk.css"
+  },
+  {
+    "revision": "5921195339c77ff4403d",
+    "url": "/static/js/2.1c6d3e48.chunk.js"
+  },
+  {
+    "revision": "c71808282eb5beabf435",
+    "url": "/static/js/main.f2760f02.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "23efb93b0bb89f1efb9e2c6b3c90d105",
+    "url": "/static/media/testpic.23efb93b.jpg"
+  }
+]);
